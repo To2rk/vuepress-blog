399b3dda20a89795de4bdc53a41953ff7c6ec11f
To2rk <ltybc1970@gmail.com>
2021-05-25 01:21:42 +0000

    GitHub Actions Auto Deploy

---
+++ blog/.vuepress/components/VtimeLine.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="about-site-timeline-content">
+      <span class="archives-m-d">{{ date }}</span>{{ title }}
+    </div>
+</template>
+
+<script>
+  export default {
+    props:['date', 'title'],
+  };
+</script>
+
+<style type="text/css">
+.about-site-timeline-content {
+    padding: 8px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.archives-m-d {
+    font-size: 14px;
+    padding-right: 20px;
+    color: rgb(134, 131, 131);
+}
+</style>
